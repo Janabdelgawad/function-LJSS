7af756a720736e00a20ef5219eba30f2c3bd6661
janaakamaal <janaakamaal2004@gmail.com>
2023-09-01 14:14:02 +0300

    revoking a function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Create two variables, firstName and lastName
-let firstName = "jana"
-let lastName = "abdelgawad"
-// Concatenate the two variables into a third variable called fullName
-let fullName = firstName + " " + lastName
-// Log fullName to the console
-console.log(fullName)
+let name = "Linda"
+let greeting = "Hi there"
+// Create a function that logs out "Hi there, Linda!" when called
 
-
+function greet(){
+    let myGreeting = greeting + ", " + name + "!"
+    console.log(myGreeting)
+}
+greet()
