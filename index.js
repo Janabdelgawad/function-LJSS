03989b551cf730dee857da7782d612dcd212225c
janaakamaal <janaakamaal2004@gmail.com>
2023-09-01 11:59:13 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+function lapCount(){
+    lapsCompleted = lapsCompleted +1
+}
+lapCount()
+lapCount()
+lapCount()
+console.log(lapsCompleted)
