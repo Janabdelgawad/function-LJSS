774f9492709478f94a49ca4b0f1f199d16ef34ee
janaakamaal <janaakamaal2004@gmail.com>
2023-09-01 12:10:19 +0300

    added html, js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked (log it out)
+// document.getElementById("count").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
 
 let count = 0
-function increment(){
+let countEl = document.getElementById("count-el")
+
+function increment() {
     count = count + 1
+    countEl.innerText = count
     console.log(count)
 }
+
+
