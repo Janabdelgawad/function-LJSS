0be4cb948f960d419992ddec32be6d67607556bf
janaakamaal <janaakamaal2004@gmail.com>
2023-09-01 12:22:56 +0300

    css , onclick js inline in html
    forgot to copy the default css code in the beginning
    learned about the built in onlick function of javascript
    inline coded it in html

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// document.getElementById("count").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-let count = 0
 let countEl = document.getElementById("count-el")
+let count = 0
 
 function increment() {
     count = count + 1
     countEl.innerText = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+
+function save(){
     console.log(count)
 }
 
