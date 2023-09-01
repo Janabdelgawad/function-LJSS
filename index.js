600312ea89017cab225a5427697917e0f2dae292
janaakamaal <janaakamaal2004@gmail.com>
2023-09-01 14:07:24 +0300

    concatinating variables

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-let countEl = document.getElementById("count-el")
-let count = 0
-
-function increment() {
-    count = count + 1
-    countEl.innerText = count
-}
-
-// 1. Create a function, save(), which logs out the count when it's called
-
-function save(){
-    console.log(count)
-}
+// Create two variables, firstName and lastName
+let firstName = "jana"
+let lastName = "abdelgawad"
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + " " + lastName
+// Log fullName to the console
+console.log(fullName)
 
 
